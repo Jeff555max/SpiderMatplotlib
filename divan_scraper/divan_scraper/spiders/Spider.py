22f7b16bdedea38deca7bcb4f9b3b595272b9802
Jeff555max <vadik98765@gmail.com>
2024-12-17 08:21:11 +0500

    add code

---
+++ divan_scraper/divan_scraper/spiders/Spider.py
@@ -0,0 +1,38 @@
+import scrapy
+
+class DivanScraperSpider(scrapy.Spider):
+    name = "divan_scraper"
+    allowed_domains = ["divan.ru"]
+    # start_urls - это та ссылка, от которой начинается парсинг
+    start_urls = ["https://www.divan.ru/category/divany"]
+
+    def parse(self, response):
+        # Создаём переменную, в которую будет сохраняться информация
+        # response.css('div.lsooF') — поиск по тегу. Будет создан целый список
+        divans = response.css('div.lsooF')
+
+        # Настраиваем работу с каждым отдельным диваном в списке
+        for divan in divans:
+            # оператор "yield" помогает обрабатывать одно отдельное действие
+            # С его помощью мы можем управлять потоком выполнения,
+            # останавливать и возобновлять работу парсера (С другими операторами мы такого делать не можем)
+            # Ссылки и теги получаем с помощью консоли на сайте
+            price = divan.css('div.fxA6s span::text').get()
+            exist = divan.css('link[itemprop="availability"]::attr(href)').get()
+
+            yield {
+                # Создаём словарик названий, используем поиск по диву, а внутри дива — по тегу span
+                'name': divan.css('div.lsooF span::text').get(),
+                # Создаём словарик цен, используем поиск по диву, а внутри дива — по тегу span
+                'price': price if exist else '0'
+            }
+
+        # Создали новый запрос для перехода на следующую страницу
+        # `self.parse` - функция, которая будет вызвана для обработки ответа с новой страницы.
+        # Мы повторно вызываем метод `parse`, чтобы извлечь данные с новой страницы точно так же, как и с первой.
+        # `yield` возвращает новый запрос в очередь Scrapy. Это асинхронный подход,
+        # который позволяет Scrapy эффективно управлять многими запросами параллельно
+        # Поиск ссылки на след страницу из атрибута href в теге <a> класс ZIuae
+        next_page = response.css('a.ZIuae::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
